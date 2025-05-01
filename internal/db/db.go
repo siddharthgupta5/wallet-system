8d20190afa3248fba03ee0d67340737d83917c31
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-01 16:40:57 +0530

    db initialized

---
+++ internal/db/db.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RespondWithError(c *gin.Context, code int, message string) {
+	c.JSON(code, gin.H{"error": message})
+}
+
+func RespondWithJSON(c *gin.Context, code int, payload interface{}) {
+	c.JSON(code, payload)
+}
+
+func ParseUintParam(c *gin.Context, param string) (uint, error) {
+	idStr := c.Param(param)
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return uint(id), nil
+}
