61f7d7268fa84c20e071a1ed567535f96cce2fd6
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-01 21:28:43 +0530

    db file modified

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,25 +1,54 @@
-package utils
+package db
 
 import (
-	"net/http"
-	"strconv"
+	"fmt"
+	"log"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/siddharthgupta5/wallet-api/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func RespondWithError(c *gin.Context, code int, message string) {
-	c.JSON(code, gin.H{"error": message})
-}
+var DB *gorm.DB
 
-func RespondWithJSON(c *gin.Context, code int, payload interface{}) {
-	c.JSON(code, payload)
-}
-
-func ParseUintParam(c *gin.Context, param string) (uint, error) {
-	idStr := c.Param(param)
-	id, err := strconv.ParseUint(idStr, 10, 32)
+// InitDB initializes the database connection
+func InitDB() {
+	err := godotenv.Load()
 	if err != nil {
-		return 0, err
+		log.Fatal("Error loading .env file")
 	}
-	return uint(id), nil
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = db
+
+	err = DB.AutoMigrate(
+		&models.User{},
+		&models.Wallet{},
+		&models.Transaction{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	log.Println("Database connection established successfully")
+}
+
+// GetDB returns the database instance
+func GetDB() *gorm.DB {
+	return DB
 }
