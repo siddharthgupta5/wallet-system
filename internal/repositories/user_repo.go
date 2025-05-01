1f8f1aef8f8e0cb6833373f8142c396388ec63a9
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-01 15:02:13 +0530

    user repo created

---
+++ internal/repositories/user_repo.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"github.com/siddharthgupta5/wallet-api/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+	FindByID(id uint) (*models.User, error)
+	FindByEmail(email string) (*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByID(id uint) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, id).Error
+	return &user, err
+}
+
+func (r *userRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
